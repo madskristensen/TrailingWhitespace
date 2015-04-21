08426fc3166b1f626358fce14c28adb0e64587ac
Mads Kristensen <post@madskristensen.net>
2015-04-21 08:38:34 -0700

    Changed content type. Fixed #2

--- src/Classifier/ClassifierProvider.cs
+++ src/Classifier/ClassifierProvider.cs
@@ -1,14 +1,15 @@
-﻿using Microsoft.VisualStudio.Text;
+﻿using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Classification;
 using Microsoft.VisualStudio.Text.Editor;
 using Microsoft.VisualStudio.Utilities;
-using System.ComponentModel.Composition;
 
 namespace TrailingWhitespace
 {
     [Export(typeof(IClassifierProvider))]
     [ContentType("text")]
-    [TextViewRole(PredefinedTextViewRoles.Editable)]
+    [TextViewRole(PredefinedTextViewRoles.Interactive)]
     class TrailingClassifierProvider : IClassifierProvider
     {
         [Import]
@@ -16,7 +17,32 @@ namespace TrailingWhitespace
 
         public IClassifier GetClassifier(ITextBuffer textBuffer)
         {
-            return textBuffer.Properties.GetOrCreateSingletonProperty(() => new TrailingClassifier(registryService, textBuffer));
+            string fileName = GetFileName(textBuffer);
+
+            // Check for file name so the classifier won't show up in tool windows etc.
+            if (!string.IsNullOrWhiteSpace(fileName))
+            {
+                return textBuffer.Properties.GetOrCreateSingletonProperty(() => new TrailingClassifier(registryService, textBuffer));
+            }
+
+            return null;
+        }
+
+        public static string GetFileName(ITextBuffer buffer)
+        {
+            Microsoft.VisualStudio.TextManager.Interop.IVsTextBuffer bufferAdapter;
+            buffer.Properties.TryGetProperty(typeof(Microsoft.VisualStudio.TextManager.Interop.IVsTextBuffer), out bufferAdapter);
+
+            if (bufferAdapter != null)
+            {
+                var persistFileFormat = bufferAdapter as IPersistFileFormat;
+                string ppzsFilename = null;
+                uint iii;
+                if (persistFileFormat != null) persistFileFormat.GetCurFile(out ppzsFilename, out iii);
+                return ppzsFilename;
+            }
+
+            return null;
         }
     }
 }
