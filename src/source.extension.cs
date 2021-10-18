502f068184d1c3a1c4e6ef2d6fb6905b5361023c
Mads Kristensen <madskristensen@users.noreply.github.com>
2021-10-18 13:45:02 -0700

    Added support for VS 2022

--- src/source.extension.cs
+++ src/source.extension.cs
@@ -7,11 +7,11 @@ namespace TrailingWhitespace
 {
     internal sealed partial class Vsix
     {
-        public const string Id = "4c1a78e6-e7b8-4aa9-8812-4836e051ff6d";
+        public const string Id = "dac582ef-fc73-42f6-ad45-afaad93d88a4";
         public const string Name = "Trailing Whitespace Visualizer";
         public const string Description = @"Keeps your code files clean by making it easier than ever to identify and remove any trailing whitespace";
         public const string Language = "en-US";
-        public const string Version = "2.5";
+        public const string Version = "2.6";
         public const string Author = "Mads Kristensen";
         public const string Tags = "whitespace";
     }
